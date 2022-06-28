e335a2cae2df4679ad2d0ebc16b26e7ca317268f
CrimsonSky <serkan-afsar@hotmail.com>
2022-06-28 21:30:08 +0300

    Context To App Init

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,13 @@
+import AppContextProvider from "../Contexts";
 import "bootstrap/dist/css/bootstrap.min.css";
 import '../styles/globals.scss'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <AppContextProvider>
+      <Component {...pageProps} />
+    </AppContextProvider>
+  )
 }
 
 export default MyApp
