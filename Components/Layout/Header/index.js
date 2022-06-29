dc23dfc506ad014b2bcd9b141cfd2dfa905f0f01
CrimsonSky <serkan-afsar@hotmail.com>
2022-06-29 13:51:45 +0300

    Header Update

--- Components/Layout/Header/index.js
+++ Components/Layout/Header/index.js
@@ -1,11 +1,30 @@
 import React from "react";
 import styles from './index.module.scss';
+import Link from "next/link";
 
 const Header = () => {
     return (
         <header className={`${styles.header} shadow`}>
             <div className="container">
-                Deneme 123
+                <nav className={styles.nav}>
+                    <Link href="/">
+                        <a className={styles.navBrand}>
+                            Ev Arkadaşı İlanları
+                        </a>
+                    </Link>
+                    <ul>
+                        <li>
+                            <a>İstanbul Ev Arkadaşı İlanları</a>
+                        </li>
+                        <li>
+                            <a>Ankara Ev Arkadaşı İlanları</a>
+                        </li>
+                        <li>
+                            <a>İzmir Ev Arkadaşı İlanları</a>
+                        </li>
+
+                    </ul>
+                </nav>
             </div>
         </header>
     )
