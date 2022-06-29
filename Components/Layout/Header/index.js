31cab86d9356c42bd1915f3bc1e5f6d546a8b0ee
CrimsonSky <serkan-afsar@hotmail.com>
2022-06-29 13:17:06 +0300

    Header Init

---
+++ Components/Layout/Header/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import styles from './index.module.scss';
+
+const Header = () => {
+    return (
+        <header className={`${styles.header} shadow`}>
+            <div className="container">
+                Deneme 123
+            </div>
+        </header>
+    )
+}
+export default Header;
