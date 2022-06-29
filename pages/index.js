056dcd4187b6cb7cfdd82e60d28b7967458049cd
CrimsonSky <serkan-afsar@hotmail.com>
2022-06-29 13:50:07 +0300

    Index Page Update

--- pages/index.js
+++ pages/index.js
@@ -3,8 +3,6 @@ import styles from '../styles/Home.module.scss'
 
 export default function Home() {
   return (
-    <div className='container'>
-      <h1>Hello World</h1>
-    </div>
+    <h1>Hello World</h1>
   )
 }
