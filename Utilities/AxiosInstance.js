ea8e704bc82e0191f558370ba8b3f874d62c4713
CrimsonSky <serkan-afsar@hotmail.com>
2022-08-22 22:45:14 +0300

    default instance added

--- Utilities/AxiosInstance.js
+++ Utilities/AxiosInstance.js
@@ -3,6 +3,13 @@ import axios from 'axios';
 
 const httpsAgent = new https.Agent({ rejectUnauthorized: false });
 
+
+export const defaultInstace = axios.create({
+    baseURL: `${process.env.NEXT_PUBLIC_API_URL}`,
+    httpsAgent: httpsAgent,
+});
+
+
 export const ApiClient = (tokenKey = null) => {
     const instance = axios.create({
         baseURL: `${process.env.NEXT_PUBLIC_API_URL}`,
