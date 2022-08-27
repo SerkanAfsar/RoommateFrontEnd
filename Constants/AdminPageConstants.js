efd3881236c199f9ac38857d3d1a1f38b9652f6f
CrimsonSky <serkan-afsar@hotmail.com>
2022-08-27 14:30:41 +0300

    Admin Router Constants Init

---
+++ Constants/AdminPageConstants.js
@@ -0,0 +1,9 @@
+export const AdminRouteNames = {
+    ANASAYFA: "Anasayfa",
+    ILLER: "Iller",
+    ONAYLIILANLAR: "Onaylı İlanlar",
+    ONAYBEKLEYENILANLAR: "Onay Bekleyen İlanlar",
+    ENGELLIILANLAR: "Engelli İlanlar",
+    BLOG: "Blog",
+    KULLANICILAR: "Kullanıcılar"
+}
