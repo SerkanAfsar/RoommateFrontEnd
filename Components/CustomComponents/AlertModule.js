a9a2c7f1a3e58fed454fd388960067e49494069a
CrimsonSky <serkan-afsar@hotmail.com>
2022-08-30 13:13:13 +0300

    Alert Module title added

--- Components/CustomComponents/AlertModule.js
+++ Components/CustomComponents/AlertModule.js
@@ -1,9 +1,10 @@
 import React from "react";
 import styles from './AlertModule.module.scss';
 
-const AlertModule = ({ items }) => {
+const AlertModule = ({ items, title }) => {
     return (
         <div className={`${styles.wrapper} alert alert-danger`} role="alert" >
+            <h4>{title}</h4>
             <ul>
                 {items?.map((item, index) => (
                     <li key={index}>{item}</li>
