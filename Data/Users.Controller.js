2da52f32f93cd4aa8722f2bc93e52afe4a8f0c7e
CrimsonSky <serkan-afsar@hotmail.com>
2022-08-30 13:14:40 +0300

    Users Controller Init

--- Data/Users.Controller.js
+++ Data/Users.Controller.js
@@ -1,2 +1,13 @@
-import { ApiClient } from "../Utilities";
+import { ApiClient, StatusCodeHelper } from "../Utilities";
+
+export const GetUserCount = async (tokenKey = null) => {
+    return await ApiClient(tokenKey).get(`/users`)
+        .then(resp => {
+            return resp.data.length;
+        }).catch(err => {
+            const result = StatusCodeHelper(err);
+            return result;
+
+        });
+}
 
