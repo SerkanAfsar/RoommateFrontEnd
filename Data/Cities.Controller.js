306b3deb5bad3856375c983f37bf9fca591d16d9
CrimsonSky <serkan-afsar@hotmail.com>
2022-09-01 13:56:53 +0300

    Post City Added

--- Data/Cities.Controller.js
+++ Data/Cities.Controller.js
@@ -10,3 +10,36 @@ export const GetCityCount = async (tokeyKey = null) => {
 
         });
 }
+export const GetCityList = async (tokenKey = null) => {
+    return await ApiClient(tokenKey).get(`/cities`)
+        .then(resp => {
+            return resp?.data?.data?.map((item, index) => {
+                return {
+                    id: item.id,
+                    CityName: item?.attributes?.CityName
+                }
+            })
+        }).catch(err => {
+            const result = StatusCodeHelper(err);
+            return result;
+
+        });
+}
+export const AddCity = async (tokenKey = null, CityName) => {
+
+    return await ApiClient(tokenKey).post(`/cities`, {
+        data: {
+            CityName
+        }
+    }).then(resp => {
+
+        return {
+            id: resp.data.data.id,
+            CityName: resp.data.data.attributes.CityName
+        }
+    }).catch(err => {
+        const result = StatusCodeHelper(err);
+        return result;
+
+    });
+}
