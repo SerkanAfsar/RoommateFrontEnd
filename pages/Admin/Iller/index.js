5a0dfa703d2236b52a9f31a706600e104efa57d9
CrimsonSky <serkan-afsar@hotmail.com>
2022-09-03 12:36:06 +0300

    Admin Iller page crud process done

--- pages/Admin/Iller/index.js
+++ pages/Admin/Iller/index.js
@@ -3,29 +3,37 @@ import { AdminRouteNames } from "@/Constants/AdminPageConstants";
 import React, { useRef, useState } from "react";
 import styles from './index.module.scss';
 import { Table, Button, Tooltip, Row, Col, Container, Modal, Input, Text } from "@nextui-org/react";
-import { AddCity, GetCityList } from "Data/Cities.Controller";
+import { AddCity, GetCityList, UpdateCity as UpdateProcess, DeleteCity as DeleteProcess } from "Data/Cities.Controller";
 import { toast } from "react-toastify";
+import { useRouter } from "next/router";
 
 const Iller = ({ cityList }) => {
 
+    const router = useRouter();
+
     const [cities, setCities] = useState(cityList);
     const [visible, setVisible] = React.useState(false);
+    const [updateVisible, setUpdateVisible] = React.useState(false);
     const [cityName, setCityName] = useState("");
+    const [updatedCity, setUpdatedCity] = useState({ CityName: "", id: 0 });
+
     const handler = () => setVisible(true);
-
-
-
+    const updateHandler = () => setUpdateVisible(true);
 
     const closeHandler = () => {
         setCityName("");
         setVisible(false);
+    };
+    const closeUpdateHandler = () => {
+        setUpdatedCity({});
+        setUpdateVisible(false);
     };
 
     const saveCity = async (e) => {
         e.preventDefault();
 
         if (!cityName) {
-            alert("Enter City Name");
+            alert("Şehir Adını Giriniz");
             return;
         }
         const responseResult = await AddCity(null, cityName);
@@ -36,14 +44,57 @@ const Iller = ({ cityList }) => {
             return;
         }
         setCities((items) => [...items, responseResult]);
+
+
         toast.success(`${responseResult.CityName} Eklendi`, { position: "top-right" });
+        router.push(window.location.href);
         closeHandler();
+
     }
 
-    console.log(cityName);
+
+    const updateCity = async (e) => {
+        e.preventDefault();
+
+        if (!updatedCity.CityName) {
+            alert("Şehir Adını Giriniz");
+            return;
+        }
+        const responseResult = await UpdateProcess(null, updatedCity.id, updatedCity.CityName);
+        if (responseResult?.hasError) {
+            responseResult?.errorList.forEach(err => {
+                toast.error(err, { position: "top-right" });
+            });
+            return;
+        }
+
+        const arr = cityList;
+        const item = arr[arr.findIndex(a => a.id == responseResult.id)];
+        item.CityName = responseResult.CityName;
+        setCities(arr);
+
+        toast.warning(`${responseResult.CityName} Güncellendi`, { position: "top-right" });
+        closeUpdateHandler();
+
+    }
+
+    const DeleteCity = async (e, cityId) => {
+        e.preventDefault();
+        const responseResult = await DeleteProcess(null, cityId);
+        if (responseResult?.hasError) {
+            responseResult?.errorList.forEach(err => {
+                toast.error(err, { position: "top-right" });
+            });
+            return;
+        }
+        toast.error(`${responseResult.CityName} Silindi`, { position: "top-right" });
+        setCities((items) => items.filter(a => a.id != responseResult.id));
+    }
+
+
     return (
         <AdminLayout activePageName={AdminRouteNames.ILLER}>
-            <Container >
+            <Container>
                 <Row justify="flex-end">
                     <Button color="success" onPress={handler}>Yeni İl Ekle</Button>
                 </Row>
@@ -70,17 +121,20 @@ const Iller = ({ cityList }) => {
                             <Table.Cell>{item.CityName}</Table.Cell>
                             <Table.Cell>
                                 <Tooltip content="İlçeler">
-                                    <Button color="secondary">İlçeler</Button>
+                                    <Button onClick={() => router.push(`/Admin/Ilceler?id=${item.id}`)} color="secondary">İlçeler</Button>
                                 </Tooltip>
                             </Table.Cell>
                             <Table.Cell>
                                 <Tooltip content="Detay">
-                                    <Button color="warning">Detay</Button>
+                                    <Button color="warning" onClick={(e) => {
+                                        setUpdateVisible(true);
+                                        setUpdatedCity(item);
+                                    }}>Detay</Button>
                                 </Tooltip>
                             </Table.Cell>
                             <Table.Cell>
                                 <Tooltip content="Sil">
-                                    <Button color="error">Sil</Button>
+                                    <Button color="error" onClick={(e) => DeleteCity(e, item.id)}>Sil</Button>
                                 </Tooltip>
                             </Table.Cell>
                         </Table.Row>
@@ -118,6 +172,38 @@ const Iller = ({ cityList }) => {
                     </Modal.Body>
                     <Modal.Footer>
                         <Button auto flat color="error" onPress={closeHandler}>
+                            Kapat
+                        </Button>
+                        <Button auto type="submit" color="success">
+                            Kaydet
+                        </Button>
+                    </Modal.Footer>
+                </form>
+            </Modal>
+            <Modal
+                closeButton
+                aria-labelledby="modal-title"
+                open={updateVisible}
+                onClose={closeUpdateHandler}
+            >
+                <form onSubmit={async (e) => await updateCity(e)}>
+                    <Modal.Body>
+                        <Input
+                            clearable
+                            bordered
+                            fullWidth
+                            type="text"
+                            color="primary"
+                            label="Şehir Adı"
+                            size="lg"
+                            placeholder="Şehir Adını Giriniz"
+                            value={updatedCity.CityName}
+                            onChange={(e) => setUpdatedCity({ ...updatedCity, CityName: e.target.value })}
+                        />
+
+                    </Modal.Body>
+                    <Modal.Footer>
+                        <Button auto flat color="error" onPress={closeUpdateHandler}>
                             Kapat
                         </Button>
                         <Button auto type="submit" color="success">
