dc7dec05e9d3826b056d90b7014a12c73037cd5d
CrimsonSky <serkan-afsar@hotmail.com>
2022-09-03 13:32:10 +0300

    Ilceler Crud Add Method Init

--- Data/Disticts.Controller.js
+++ Data/Disticts.Controller.js
@@ -1,7 +1,8 @@
+
 import { ApiClient, StatusCodeHelper } from "../Utilities";
 
-export const GetDistictCount = async (tokeyKey = null) => {
-    return await ApiClient(tokeyKey).get(`/districts`)
+export const GetDistictCount = async (tokenKey = null) => {
+    return await ApiClient(tokenKey).get(`/districts`)
         .then(resp => {
             return resp.data.meta.pagination.total;
         }).catch(err => {
@@ -9,4 +10,36 @@ export const GetDistictCount = async (tokeyKey = null) => {
             return result;
         });
 
+}
+export const GetCityWithDistricts = async (tokenKey = null, cityId) => {
+    return await ApiClient(tokenKey).get(`/cities/${cityId}?populate[0]=districts`)
+        .then(resp => {
+            return {
+                CityId: resp.data.data.id,
+                CityName: resp.data.data.attributes.CityName,
+                DisctrictList: resp.data.data.attributes.districts.data.map((item, index) => {
+                    return {
+                        DisctrictId: item.id,
+                        DistrictName: item.attributes.DistrictName
+                    }
+                })
+            }
+        }).catch(err => {
+            const result = StatusCodeHelper(err);
+            return result;
+        });
+
+}
+export const AddDisctrict = async (tokenKey, data) => {
+    return await ApiClient(tokenKey).post(`/districts`, {
+        data
+    }).then(resp => {
+        return {
+            DistrictName: resp.data.data.attributes.DistrictName,
+            DisctrictId: resp.data.data.id
+        }
+    }).catch(err => {
+        const result = StatusCodeHelper(err);
+        return result;
+    });
 }
