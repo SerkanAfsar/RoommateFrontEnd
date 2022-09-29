b2af6cea99deeb51fa0402e339eb4089e85325db
CrimsonSky <serkan-afsar@hotmail.com>
2022-09-29 13:10:25 +0300

    Header Component Added

--- Components/Layout/index.js
+++ Components/Layout/index.js
@@ -3,14 +3,17 @@ import Content from "./Content";
 import Footer from "./Footer";
 import Header from "./Header";
 import styles from './index.module.scss';
+import { Container } from "@nextui-org/react";
 
 const Layout = ({ children }) => {
     return (
         <>
             <Header />
-            <Content>
-                {children}
-            </Content>
+            <Container>
+                <Content>
+                    {children}
+                </Content>
+            </Container>
             <Footer />
         </>
     );
